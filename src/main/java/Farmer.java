2615800af78ce9cbf058d8b4f4b601c68a702a9b
Amundy98 <aisha.mundy@gmail.com>
2022-04-07 12:04:06 -0400

    removed an implementation

--- src/main/java/Farmer.java
+++ src/main/java/Farmer.java
@@ -1,15 +1,15 @@
-public class Farmer extends Person implements IRider, IBotanist , IRideable{
+public class Farmer extends Person implements IRider, IBotanist{
 
 
 //    private Crop c;
 
 
-    public void mount(Rideable rideable){
+    public void mount(IRideable rideable){
         System.out.println("Im going to set up my" + rideable);
     }
 
 
-    public void dismount(Rideable rideable){
+    public void dismount(IRideable rideable){
         System.out.println("Let me go get off my" + rideable);
     }
 

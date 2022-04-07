749bfeb7b956c3beb9a9a32dc75b6ad7cea822c5
myhomeworkfone <adzadurenne@gmail.com>
2022-04-07 16:57:10 -0400

    ha change it

---
+++ src/main/java/ChickenCoop.java
@@ -0,0 +1,9 @@
+public class ChickenCoop {
+    //stores many chicken
+    // i was going to extend it to chicken
+
+    public void stores (Chicken chicken){
+        System.out.println("stores"+ chicken);
+
+    }
+}
---
+++ src/main/java/CropRow.java
@@ -0,0 +1,14 @@
+public class CropRow {
+
+    //can Store Many crops
+    // i was going to extend it t crops
+    private Crop crop;
+
+    public Crop getCrop() {
+        return crop;
+    }
+
+    public void setCrop(Crop crop) {
+        this.crop = crop;
+    }
+}

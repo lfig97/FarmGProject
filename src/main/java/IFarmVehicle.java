906f19d85327dbfe45b1bba76e8421944f882336
Luis Figueroa <lfigueroa002@my.wilmu.edu>
2022-04-07 12:58:19 -0400

    Adding stable and tractor

--- src/main/java/IFarmVehicle.java
+++ src/main/java/IFarmVehicle.java
@@ -1,2 +1,3 @@
 public interface IFarmVehicle {
+    void operate(Farm f);
 }
---
+++ src/main/java/Tractor.java
@@ -0,0 +1,10 @@
+public class Tractor implements IFarmVehicle{
+    @Override
+    public void operate(Farm f) {
+        System.out.println("Operating on "+f);
+    }
+
+    public void harvest(Crop c){
+        System.out.println("Harvesting "+c);
+    }
+}

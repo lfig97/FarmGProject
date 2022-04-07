e4c25c835ea0a3b7246bd5ccf80d73d78e131049
Isaiah Holden <holdenisaiah4@gmail.com>
2022-04-07 11:31:57 -0400

    added chicken and horse classes

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,45 +1,6 @@
-public class Animal {
+public class Animal{
     private String Animal;
-
     private String Eater;
-
-
-
-    public void makeNoise(){
-        System.out.println("roar");
-    }
-
-
-    public static void main(String[] args) {
-
-
-        Animal myAnimal = new Animal();
-        myAnimal.makeNoise();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
